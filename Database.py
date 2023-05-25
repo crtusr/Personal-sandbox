cb56e6d34d67dc1a8b9f9ef9c7966bee88a029fa
crtusr <lucasgasperi03@gmail.com>
2023-05-25 13:39:04 -0300

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,12 +1,22 @@
 import sqlite3
+import os
 
-conn = sqlite3.connect('my_database.db')
-c = conn.cursor()
+# Create a database file named "mydb.db" in the current directory
+db_file = os.path.join(os.getcwd(), "mydb.db")
 
-c.execute('''CREATE TABLE IF NOT EXISTS comments
-             (id INTEGER PRIMARY KEY, content TEXT)''')
+# Connect to the database
+conn = sqlite3.connect(db_file)
 
-c.execute("INSERT INTO comments (content) VALUES ('Hello, World!')")
+# Create a cursor object to execute queries
+cur = conn.cursor()
 
+# Create a table named "users" with two columns: "name" and "age"
+cur.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)")
+
+# Insert some data into the table
+cur.execute("INSERT INTO users VALUES ('Alice', 25)")
+cur.execute("INSERT INTO users VALUES ('Bob', 30)")
+
+# Commit the changes and close the connection
 conn.commit()
 conn.close()

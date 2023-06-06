03c5de551999d4208b33668e67213a2d1edc4f02
crtusr <lucasgasperi03@gmail.com>
2023-06-06 13:09:28 -0300

    Update Adder.py

--- Adder.py
+++ Adder.py
@@ -1,9 +1,11 @@
 # Prompt the user to enter two numbers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = input("Enter the first number: "))
+num2 = input("Enter the second number: "))
 
 # Add the two numbers
-sum = num1 + num2
+sum = int(num1) + int(num2)
+add = num1 + num2
 
 # Print the result
-print("The sum of", num1, "and", num2, "is", sum)
+print(num1, "+", num2, "=", sum)
+print(num1, "+", num2, "=", sum, " (no int())")
